8a4215f5f11fef067111e33622bb2e882a972e16
Alexander Shar <alahan05@gmail.com>
2022-10-21 09:23:34 +0300

    1st task ended

---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//сколько чисел больше 0 ввёл пользователь. Разделить ввод данных, от расчета данных 
+//(Одна функция вводит данные, другая функция подсчитывает количество положительных)
+//1, -7, 567, 89, 223-> 3
+//0, 7, 8, -2, -2 -> 2
+
+int Prompt()
+{
+    System.Console.WriteLine("Write number");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a; 
+}
+int[] Array(int len)
+{
+    int[] array = new int [len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = Prompt();
+    }
+    return array;
+}
+int Count( int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count++;
+    }
+    System.Console.WriteLine($" Положительных чисел {count}");
+    return count;
+}
+
+int length = Prompt();
+int[] numbers = Array(length);
+Count( numbers);
+
